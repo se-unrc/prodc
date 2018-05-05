11680c8fcbda04e1cfd615a63e05d18137af330c
romm <roman_mag1993@hotmail.com>
2018-05-05 15:52:22 -0300

    Creacion de Tablas y Algunos Test

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -15,11 +15,11 @@ public class App
     public static void main( String[] args )
     {
 
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true", "franco", "franco");
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true", "root", "root");
 
         User u = new User();
-        u.set("username", "Riquelme");
-        u.set("password", "password");
+        u.set("username", "pepito1");
+        u.set("password", "pepito");
         u.saveIt();
 
         Base.close();
--- src/test/java/prode/UserTest.java
+++ src/test/java/prode/UserTest.java
@@ -10,25 +10,39 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class UserTest {
-  @Before
-  public void before(){
-    Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode_test?nullNamePatternMatchesAll=true", "franco", "franco");
-    System.out.println("UserTest setup");
-    Base.openTransaction();
-  }
+  	@Before
+  	public void before(){
+    	Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode_test?nullNamePatternMatchesAll=true", "root", "root");
+    	System.out.println("UserTest setup");
+    	Base.openTransaction();
+  	}
 
-  @After
-  public void after(){
-      System.out.println("UserTest tearDown");
-      Base.rollbackTransaction();
-      Base.close();
-  }
+  	@After
+  	public void after(){
+    	System.out.println("UserTest tearDown");
+    	Base.rollbackTransaction();
+    	Base.close();
+  	}
 
-  @Test
-  public void validatePrecenseOfUsernames(){
-      User user = new User();
-      user.set("username", "");
+  	@Test
+ 	public void validatePrecenseOfUsernames(){
+      	User user = new User();
+      	user.set("username", "");
+    	assertEquals(user.isValid(), false);
+  	}
 
-      assertEquals(user.isValid(), false);
-  }
+  	@Test
+  	public void validatePrecenseOfPassword(){
+    	User user = new User();
+    	user.set("password", "");
+		assertEquals(user.isValid(), false);
+  	}
+
+  	@Test
+  	public void validatePasswordSize(){
+    	User user = new User();
+    	pass = user.get();
+    	if (pass.size() <= 5) 
+    		assertEquals(user.isValid(), false);
+	}
 }
