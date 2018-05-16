2d030c651d2459f89fcc837dbe58f14fe285a997
josue <josue_menendez@hotmail.com>
2018-05-16 16:57:13 -0300

    borrado de tablas singulares

--- src/migrations/20180426185332_create_team_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE team (
-	cod_equipo int,
-	nom_equipo varchar(25),
-CONSTRAINT cp_cod_equipo PRIMARY KEY (cod_equipo)
-);
--- src/migrations/20180504115337_create_schadule_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE schadule (
-	num_fecha int,
-	cod_partido int,
-CONSTRAINT cp_num_fecha PRIMARY KEY (num_fecha, cod_partido),
-CONSTRAINT cf_cod_match FOREIGN KEY (cod_partido)
-	REFERENCES matchs (cod_partido)
-);
