0c87f805f704599b269f888346caf5a8d2a76c11
Ramiro Arivilca <jpenigmatic@gmail.com>
2018-05-18 15:00:17 -0300

    fixed spark

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -1,26 +1,32 @@
 package prode;
 
+import org.javalite.activejdbc.Base;
+import prode.User;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
-import org.javalite.activejdbc.Base;
-import prode.User;
 import static spark.Spark.*;
 
+
+
+/**
+ * Hello world!
+ *
+ */
 public class App
 {
     public static void main( String[] args )
     {
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "templates/Dashboard/hello.vtl");
+          }, new VelocityTemplateEngine());
 
         // Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true", "root", "root");
         // User u = new User();
-        // u.set("username", "Ramiro");
+        // u.set("username", "Riquelme");
         // u.set("password", "password");
         // u.saveIt();
-        staticFileLocation("/public");
-        get("/", (request, response) -> {
-           return new ModelAndView(new HashMap(), "templates/Dashboard/hello.vtl");
-         }, new VelocityTemplateEngine());
         // Base.close();
         // System.out.println( "Hello World!" );
     }
