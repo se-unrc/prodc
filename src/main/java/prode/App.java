e90796a711b0dbebc31fb4312a6d806f331db25c
Ramiro <ramiro1935@gmail.com>
2018-06-06 15:11:41 -0300

    User modelmodified

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -16,21 +16,20 @@ public class App
         // port(4444);
         // staticFiles.location("/public");
 
-        Map map = new HashMap();
-             map.put("name", "Sam");
-             map.put("value", 1000);
-             map.put("taxed_value", 1000 - (1000 * 0.4));
-             map.put("in_ca", true);
-
-             get("/hello", (req, res) -> {
-                 return new ModelAndView(map, "./Dashboard/hello.mustache");
-               }, new MustacheTemplateEngine()
-             );
-
-        // Base.open();
-        // User u = new User("Ramiro","ramiro1935@gmail.com","123456", new Fixture("Copa"));
-        // u.saveIt();
-        // Base.close();
-        // System.out.println( "Hello World!" );
+        // Map map = new HashMap();
+        //      map.put("name", "Sam");
+        //      map.put("value", 1000);
+        //      map.put("taxed_value", 1000 - (1000 * 0.4));
+        //      map.put("in_ca", true);
+        //
+        //      get("/hello", (req, res) -> {
+        //          return new ModelAndView(map, "./Dashboard/hello.mustache");
+        //        }, new MustacheTemplateEngine()
+        //      );
+        //
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode_test?nullNamePatternMatchesAll=true", "root", "root");
+        User u = new User("ramiro","ramiro1935@gmail.com","123456");
+        System.out.println(u.getComplete());
+        Base.close();
     }
 }
--- src/main/java/prode/User.java
+++ src/main/java/prode/User.java
@@ -5,18 +5,23 @@ import org.javalite.activejdbc.Model;
 public class User extends Model {
 
     static {
-      validatePresenceOf("username");
+      validatePresenceOf();
     }
 
-    // public User() {}
-    //
-    // public User(String name, String email, String password, Fixture fixture){
-    //   set("name",name);
-    //   set("email",name);
-    //   set("password",name);
-    // }
-    //
-    // public String getName(){ return getString("name"); }
-    // public String getemail(){ return getString("email"); }
-    // public String getpassword(){ return getString("password"); }
+    public User() {}
+
+    //Registro de usuarios
+    public User(String name, String email, String password){
+      set("nick",name);
+      set("email",email);
+      set("password",password);
+      set("type",0);
+      set("score",0);
+      saveIt();
+    }
+
+    public String getName(){ return getString("nick"); }
+    public String getemail(){ return getString("email"); }
+    public String getpassword(){ return getString("password"); }
+
 }
