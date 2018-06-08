bccea53efd4998610d531f262dfdf19a5931fb9a
Ramiro <ramiro1935@gmail.com>
2018-06-08 14:36:45 -0300

    update usercontroller

--- src/main/java/Services/UserService.java
+++ src/main/java/Services/UserService.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.LazyList;
 import org.javalite.activejdbc.Model;
+import static spark.Spark.*;
 
 import prode.User;
 
@@ -13,39 +14,40 @@ public class UserService {
 	private Map<String, User> users = new HashMap<>();
 
 	public List<User> getAllUsers() {
-    Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode_test?nullNamePatternMatchesAll=true", "root", "root");
+
     List<User> u = User.findAll();
     Base.close();
     return u;
 	}
 
-	public User getUser(String id) {
+	public User getUser(String nick) {
     Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode_test?nullNamePatternMatchesAll=true", "root", "root");
-    LazyList<Model> u = User.where("nick = ?", id);
-    User usr =  (u!= null)?(User)u.get(0):null;
+    User usr = User.findFirst("nick = ?",nick);
+    //LazyList<Model> u = User.where("nick = ?", nick);
+    //User usr =  (u!= null)?(User)u.get(0):null;
     Base.close();
 	  return usr;
 	}
 
 	// public User createUser(String name, String email) {
-	// 	failIfInvalid(name, email);
+	// 	failIfInvalnick(name, email);
 	// 	User user = new User(name, email);
-	// 	users.put(user.getId(), user);
+	// 	users.put(user.getnick(), user);
 	// 	return user;
 	// }
-  // 
-	// public User updateUser(String id, String name, String email) {
-	// 	User user = users.get(id);
+  //
+	// public User updateUser(String nick, String name, String email) {
+	// 	User user = users.get(nick);
 	// 	if (user == null) {
-	// 		throw new IllegalArgumentException("No user with id '" + id + "' found");
+	// 		throw new IllegalArgumentException("No user with nick '" + nick + "' found");
 	// 	}
-	// 	failIfInvalid(name, email);
+	// 	failIfInvalnick(name, email);
 	// 	user.setName(name);
 	// 	user.setEmail(email);
 	// 	return user;
 	// }
   //
-	// private void failIfInvalid(String name, String email) {
+	// private vonick failIfInvalnick(String name, String email) {
 	// 	if (name == null || name.isEmpty()) {
 	// 		throw new IllegalArgumentException("Parameter 'name' cannot be empty");
 	// 	}
