10a55dfc43c19c22bea45eddda0d599e641e7ff6
romm <roman_mag1993@hotmail.com>
2018-06-24 20:40:31 -0300

    Modificacion de la Pagina

--- src/main/java/prode/Prediction.java
+++ src/main/java/prode/Prediction.java
@@ -16,4 +16,20 @@ public class Prediction extends Model {
 		p.set("equipoL", equipoL);
 		p.set("equipoV", equipoV);
 	}
+
+	public Map addPrediction(Request req){
+		String golloc = req.queryParams("elocal");
+		String golvis = req.queryParams("evisit");
+
+		int golLocal = (int)golloc.charAt(0);
+		int golVisit = (int)golvis.charAt(0);
+
+		Map pred = new HashMap();
+		Prediction p = new Prediction();
+		p.set("equipoL", golLocal);
+		p.set("equipoV", golVisit);
+		p.saveIt();
+		pred.put("exito", "Predicción realizada! MUCHA SUERTE!");
+		return pred;
+	}
 }
