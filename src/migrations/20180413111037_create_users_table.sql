cc90023016f7ce1660ad781ace5348e2e75bebfc
josue <josue_menendez@hotmail.com>
2018-09-22 18:21:24 -0300

    changend database

--- src/migrations/20180413111037_create_users_table.sql
+++ src/migrations/20180413111037_create_users_table.sql
@@ -1,9 +1,8 @@
 CREATE TABLE users (
   id int auto_increment PRIMARY KEY,
   username varchar(126),
-  password varchar(126);
+  password varchar(126),
   superu BOOLEAN,
   created_at DATETIME,
-  updated_at
-  )
-)
+  updated_at DATETIME
+  );
