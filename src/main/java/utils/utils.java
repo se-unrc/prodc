ab09ccea0c42b0dbd8aa67220d22290533e9a0d1
Carlos Esteban <37701947+EstebanNiebylski@users.noreply.github.com>
2018-10-29 18:21:52 -0300

    Implementing metrics

--- src/main/java/utils/utils.java
+++ src/main/java/utils/utils.java
@@ -1,5 +1,30 @@
 package utils;
 
+import com.codahale.metrics.*;
+import java.util.concurrent.TimeUnit;
+
 public class Metrics{
+
+static final MetricRegistry metrics = new MetricRegistry();
+
+public static void main(String args[]) {
+    startReport();
+    Meter requests = metrics.meter("requests");
+    requests.mark();
+    wait5Seconds();
+    }
+
+    static void startReport() {
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+        .convertRatesTo(TimeUnit.SECONDS)
+        .convertDurationsTo(TimeUnit.MILLISECONDS)
+        .build();
+        reporter.start(1, TimeUnit.SECONDS);
+    }
+
+    static void wait3Seconds() {
+        try { Thread.sleep(3*1000);}
+        catch(InterruptedException e) {}
+    }
 
 }
