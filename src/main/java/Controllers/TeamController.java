0730b3669451a07f645ab6f6b96316030594f07c
susiblanco <susimblanco@gmail.com>
2018-11-21 20:40:29 -0300

    creado el metodo post de Equipo

--- src/main/java/Controllers/TeamController.java
+++ src/main/java/Controllers/TeamController.java
@@ -10,8 +10,9 @@ import java.util.HashMap;
 import org.javalite.activejdbc.Base;
 
 import Dao.*;
-import Model.*;
+import Model.Team;
 import spark.ModelAndView;
+
 
 public class TeamController {
 
@@ -21,6 +22,21 @@ public class TeamController {
 	get("/team/new",(req, res)->{
 	return new ModelAndView (null, "./Dashboard/teamcreate.mustache");
 	}, new MustacheTemplateEngine());
+	
+	//Creacion de usuarios
+		post("/team/save", (req, res) -> {
+			
+			Map map = new HashMap();
+			String teamname = req.queryParams("nombre");
+			Team team = new Team(teamname);
+			String mensaje ="Se creo Equipo exitosamente";
+			
+			if(team == null){
+				mensaje = "Error no se pudo cargar el equipo";
+			}
+			map.put("mensaje",mensaje);
+			return new ModelAndView(map, "./Dashboard/teamcreate.mustache");
+		}, new MustacheTemplateEngine());
 	
 
   }
